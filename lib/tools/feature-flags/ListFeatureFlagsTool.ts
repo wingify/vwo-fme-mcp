efa406f768b89eb63a9ae545f998be4ca5bb8f4c
softvar <varun2902@gmail.com>
2025-05-18 18:09:44 +0530

    feat: list feature flag tool

---
+++ lib/tools/feature-flags/ListFeatureFlagsTool.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2025 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ListFeatureFlagsSchema } from '../../schema/ListFeatureFlagsSchema';
+import { BaseTool } from '../BaseTool';
+import { VWORestAPI } from '../../services/VWORestAPI';
+export class ListFeatureFlagsTool extends BaseTool {
+  constructor() {
+    const name = 'ListFeatureFlags';
+    const description = 'List all feature flags';
+    const inputSchema = ListFeatureFlagsSchema;
+
+    super(name, description, inputSchema);
+  }
+
+  /**
+   * List all feature flags
+   * @param args - The arguments for the tool
+   * @returns The result of the tool
+   */
+  async execute(args: { limit?: number; offset?: number }): Promise<any> {
+    console.error('Getting list of feature flags', args);
+    const result = await VWORestAPI.Instance.listFeatureFlags(args.limit, args.offset);
+    console.error('List of feature flags', result);
+    return {
+      content: [
+        {
+          type: 'text',
+          text: `Feature flags fetched successfully. Result: ${JSON.stringify(result)}`,
+        },
+      ],
+    };
+  }
+}
