8849a2ee8468f47078c5a4197058dc3e1a3874dd
Abhi591 <abhi37716@gmail.com>
2025-05-27 19:55:08 +0530

    feat: toggle feature flag and rules tool

---
+++ lib/tools/feature-flag-rules/ToggleFeatureFlagRuleTool.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2025 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ToggleFeatureFlagAndRulesSchema } from '../../schema/ToggleFeatureFlagAndRulesSchema';
+import { BaseTool } from '../BaseTool';
+import { VWORestAPI } from '../../services/VWORestAPI';
+
+export class ToggleFeatureFlagRuleTool extends BaseTool {
+  constructor() {
+    const name = 'ToggleFeatureFlagRule';
+    const description = 'Toggle a feature flag rule';
+    const inputSchema = ToggleFeatureFlagAndRulesSchema;
+
+    super(name, description, inputSchema);
+  }
+
+  /**
+   * Toggle a feature flag rule
+   * @param args - The arguments for the tool
+   * @returns The result of the tool
+   */
+  async execute(args: {
+    environmentIdOrKey: string;
+    featureIdOrKey: string;
+    ruleIdOrKey: string;
+    isEnabled: boolean;
+  }): Promise<any> {
+    console.error('Toggling feature flag rule', args);
+    const result = await VWORestAPI.Instance.toggleFeatureFlagRule(
+      args.environmentIdOrKey,
+      args.featureIdOrKey,
+      args.ruleIdOrKey,
+      args.isEnabled,
+    );
+    return {
+      content: [
+        {
+          type: 'text',
+          text: `Feature flag rule "${args.ruleIdOrKey}" toggled successfully. Result: ${JSON.stringify(result)}`,
+        },
+      ],
+    };
+  }
+}
