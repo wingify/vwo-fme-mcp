4da8774068b99e632558d12aa89f05f5316a06bd
Abhi591 <abhi37716@gmail.com>
2025-05-30 19:22:01 +0530

    feat: adding server file with all the tools

---
+++ lib/server.ts
@@ -0,0 +1,124 @@
+/**
+ * Copyright 2025 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+
+// @ts-ignore
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { VWOConfig } from './config/index';
+import { URL } from './constants';
+
+import { VWORestAPI } from './services/VWORestAPI';
+import {
+  BootStrapVWO,
+  CreateFeatureFlagTool,
+  DeleteFeatureFlagTool,
+  UpdateFeatureFlagTool,
+  GetFeatureFlagTool,
+  ListFeatureFlagsTool,
+  ListFeatureFlagRulesTool,
+  CreateRolloutAndPersonalizeRuleTool,
+  CreateTestingAndMVTRuleTool,
+  GetFeatureFlagRuleTool,
+  ToggleFeatureFlagRuleTool,
+  ToggleFeatureFlagTool,
+  DeleteFeatureFlagRuleTool,
+  ListProjectsAndEnvironmentsTool,
+  GetMetricsTool,
+} from './tools';
+
+export class VWOMCPServer {
+  public config: {
+    name: string;
+    version: string;
+  };
+  private server: McpServer;
+  private api: any;
+  private tools: Map<string, any>;
+
+  constructor(config: any) {
+    this.server = new McpServer(
+      {
+        name: config.name,
+        version: config.version,
+      },
+      {
+        capabilities: {
+          resources: {}, // Enable resources
+        },
+      },
+    );
+
+    this.config = {
+      name: config.name,
+      version: config.version,
+    };
+
+    this.api = new VWORestAPI({
+      baseURL: URL.VWO_API,
+      apiKey: VWOConfig.apiKey,
+      accountId: VWOConfig.accountId,
+    });
+
+    this.tools = new Map();
+    this.registerTools();
+  }
+
+  private registerTools() {
+    const tools = [
+      new BootStrapVWO(),
+
+      new ListFeatureFlagsTool(),
+      new CreateFeatureFlagTool(),
+      new GetFeatureFlagTool(),
+      new UpdateFeatureFlagTool(),
+      new DeleteFeatureFlagTool(),
+
+      new ListProjectsAndEnvironmentsTool(),
+
+      new ListFeatureFlagRulesTool(),
+      new CreateRolloutAndPersonalizeRuleTool(),
+      new CreateTestingAndMVTRuleTool(),
+      new GetFeatureFlagRuleTool(),
+      new ToggleFeatureFlagRuleTool(),
+      new ToggleFeatureFlagTool(),
+      new DeleteFeatureFlagRuleTool(),
+
+      new GetMetricsTool(),
+    ];
+
+    tools.forEach((tool) => {
+      this.registerTool(tool);
+    });
+  }
+
+  public registerTool(tool: any): void {
+    this.server.tool(tool.name, tool.description, tool.inputSchema, tool.execute.bind(tool));
+  }
+
+  public async connect(transport: any): Promise<void> {
+    await this.server.connect(transport);
+    console.error(`${this.config.name} MCP Server running on stdio`);
+  }
+
+  public async start() {
+    const transport = new StdioServerTransport();
+    await this.connect(transport);
+  }
+}
---
+++ lib/tools/index.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2025 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export { BootStrapVWO } from './fetch-cursor-rules/BootStrapVWO';
+
+export { CreateFeatureFlagTool } from './feature-flags/CreateFeatureFlagTool';
+export { DeleteFeatureFlagTool } from './feature-flags/DeleteFeatureFlagTool';
+export { GetFeatureFlagTool } from './feature-flags/GetFeatureFlagTool';
+export { ListFeatureFlagsTool } from './feature-flags/ListFeatureFlagsTool';
+export { UpdateFeatureFlagTool } from './feature-flags/UpdateFeatureFlagTool';
+
+export { CreateRolloutAndPersonalizeRuleTool } from './feature-flag-rules/CreateRolloutAndPersonalizeRuleTool';
+export { CreateTestingAndMVTRuleTool } from './feature-flag-rules/CreateTestingAndMVTRuleTool';
+export { GetFeatureFlagRuleTool } from './feature-flag-rules/GetFeatureFlagRuleTool';
+export { ListFeatureFlagRulesTool } from './feature-flag-rules/ListFeatureFlagRulesTool';
+export { ToggleFeatureFlagRuleTool } from './feature-flag-rules/ToggleFeatureFlagRuleTool';
+export { ToggleFeatureFlagTool } from './feature-flags/ToggleFeatureFlagTool';
+export { DeleteFeatureFlagRuleTool } from './feature-flag-rules/DeleteFeatureFlagRuleTool';
+
+export { GetMetricsTool } from './metrics/GetMetricsTool';
+
+export { ListProjectsAndEnvironmentsTool } from './projects-n-environments/ListProjectsAndEnvironmentsTool';
